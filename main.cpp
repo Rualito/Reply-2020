fd9e6b769d1017fc7c627b96d12ac7b7ed492640
Afonso <afonso2@Afonsos-MacBook-Air.local>
2020-03-12 16:31:12 +0000

    aaa

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
-
+#include <iostream>
 #include <cstdlib>
+#include "Replyer.h"
+
 
 int main(void){
-    
+    Replyer testReplyer("opn 7 2 java bpm");
+    std::cout<<testReplyer.C<<'\n';
     return 0;
 }
